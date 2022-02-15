2927436c841ff6869eb5b3f7e5e5eac583065315
yuuchin <shontzuforever@gmail.com>
2022-02-15 19:03:39 +0800

    google analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'  // Add this
-import router from './router'       // Add this
+import VueRouter from 'vue-router' 
+import router from './router'  
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueRouter)
+
+Vue.use(VueAnalytics, {
+    id: '303633791',
+    router
+})
 
 new Vue({
   el: '#app',
