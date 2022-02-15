add440ddaecbaeb392120c59ace04836e42729ee
yuuchin <shontzuforever@gmail.com>
2022-02-15 19:48:57 +0800

    remove inline aos

--- src/App.vue
+++ src/App.vue
@@ -22,21 +22,6 @@ export default {
   margin-top: 60px;
 }
 
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
 a {
   color: #42b983;
 }
